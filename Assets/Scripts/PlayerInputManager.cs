0ed0956a2889db4f41b7a22dcb231e117bca5cd3
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-01 18:26:19 -0700

    Player Movements
    1) PlayerInputManager will call the PlayerMove() method every fixed update with the Vector2 argument generated by the bindings that were set up and pressed.

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -6,10 +6,17 @@ using UnityEngine.InputSystem;
 public class PlayerInputManager : MonoBehaviour
 {
     private PlayerInput playerInput;
+    private PlayerController controller;
 
     private void Awake()
     {
         playerInput = new PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        // Tell the player controller to move using the value from our movement action.
+        controller.PlayerMove(playerInput.OnFoot.Movement.ReadValue<Vector2>());
     }
 
     private void OnEnable()
