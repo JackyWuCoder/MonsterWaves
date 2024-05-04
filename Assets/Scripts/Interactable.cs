7fa3f59ed98848ec3352abf49569428ce9cc7cd4
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-03 17:46:40 -0700

    Interactable Script Created
    1) This script belongs to any interactable objects and uses the template method design pattern.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    // The message displayed to player when looking at an interactable object.
+    [SerializeField] private string promptMessage;
+
+    // Template Method Design Pattern
+    // This function will be called from the player.
+    public void BaseInteract()
+    {
+        Interact();
+    }
+
+    protected virtual void Interact()
+    { 
+        // We wont have any code written in this function.
+        // This is a template function to be overridden by our subclasses.
+    }
+}
