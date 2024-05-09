93f2c0974cbea552c475ae2916921febc0b29aa4
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-09 16:19:38 -0700

    Zombie Head Explode Improved

--- Assets/Scripts/Enemy/ZombieHead.cs
+++ Assets/Scripts/Enemy/ZombieHead.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class ZombieHead : MonoBehaviour
 {
+    [SerializeField] private List<Rigidbody> allParts = new List<Rigidbody>();
     [SerializeField] private GameObject headReplacement;
     [SerializeField] private GameObject headToDestroy;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -13,6 +20,12 @@ public class ZombieHead : MonoBehaviour
         if (!collision.gameObject.CompareTag("Bullet"))
             return;
         GameObject.Instantiate(headReplacement, transform.position, transform.rotation);
+        foreach (Rigidbody part in allParts)
+        {
+            part.isKinematic = false;
+            part.AddExplosionForce(100.0f, transform.position, 5.0f);
+        }
         Destroy(headToDestroy);
+        animator.Play("Z_FallingBack");
     }
 }
