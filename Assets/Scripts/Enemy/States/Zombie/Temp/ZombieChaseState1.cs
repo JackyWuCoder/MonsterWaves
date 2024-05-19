f7ec00fe8311efa7aa6c370b0bb0a0a30edc85ae
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-19 16:05:24 -0700

    Update ZombieChaseState1.cs

--- Assets/Scripts/Enemy/States/Zombie/Temp/ZombieChaseState1.cs
+++ Assets/Scripts/Enemy/States/Zombie/Temp/ZombieChaseState1.cs
@@ -27,10 +27,23 @@ public class ZombieChaseState1 : StateMachineBehaviour
         animator.transform.LookAt(player);
 
         float distanceFromPlayer = Vector3.Distance(player.position, animator.transform.position);
+
+        // Check if agent should stop chasing
+        if (distanceFromPlayer > stopChasingDistance)
+        {
+            animator.SetBool("isChasing", false);
+        }
+
+        // Check if agent should attack
+        if (distanceFromPlayer < attackingDistance)
+        {
+            animator.SetBool("isAttacking", true);
+        }
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        // Stops the agent from moving
+        agent.SetDestination(animator.transform.position);
     }
 }
