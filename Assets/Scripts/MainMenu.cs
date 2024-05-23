04f9affa43cf06a68b976814c8d110434f1b25cf
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-22 21:33:45 -0700

    Read/Write Prefabs Inabled
    1) Fixes the Play Button Errors

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text highScoreUI;
+    private string newGameScene = "Map_v1";
+
     void Start()
     {
-        
+        // Ensure SaveLoadManager.Instance is not null before calling LoadHighScore
+        if (SaveLoadManager.Instance != null)
+        {
+            // Set the high score text
+            int highScore = SaveLoadManager.Instance.LoadHighScore();
+            highScoreUI.text = $"Highest Waves Survived: {highScore}";
+        }
+        else
+        {
+            Debug.LogError("SaveLoadManager.Instance is null. Make sure SaveLoadManager is initialized correctly.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartNewGame()
     {
-        
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void ExitApplication()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
     }
 }
